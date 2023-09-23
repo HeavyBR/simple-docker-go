75c4fd327389a2895b6c12e408c9fb43bd43a7be
Matheus Cumpian <matheus.cumpian@hotmail.com>
2023-09-23 11:26:03 -0300

    pass 5st stage

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"os/exec"
 )
@@ -10,6 +11,26 @@ import (
 func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
+
+	// Create temp dir
+	if err := exec.Command("mkdir", "-p", "/tmp/your_docker").Run(); err != nil {
+		os.Exit(1)
+	}
+
+	// Copy the command binary to temp dir
+	commandPath, err := exec.LookPath(command)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if err := exec.Command("cp", commandPath, fmt.Sprintf("/tmp/your_docker/%s", command)).Run(); err != nil {
+		os.Exit(1)
+	}
+
+	// CHRoot to temp dir
+	if err := exec.Command("chroot", "/tmp/your_docker").Run(); err != nil {
+		os.Exit(1)
+	}
 
 	cmd := exec.Command(command, args...)
 	pipeIO(cmd)
