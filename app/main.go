c153b0ddad84783c78e4dba5d14dfc616a3d08ab
Matheus Cumpian <matheus.cumpian@hotmail.com>
2023-09-25 00:16:30 -0300

    commenting

--- app/main.go
+++ app/main.go
@@ -1,23 +1,198 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"syscall"
+	"time"
 )
+
+var client = &http.Client{
+	Timeout: 10 * time.Second,
+}
+
+type ManifestResponse struct {
+	SchemaVersion int    `json:"schemaVersion"`
+	MediaType     string `json:"mediaType"`
+	Config        struct {
+		MediaType string `json:"mediaType"`
+		Size      int    `json:"size"`
+		Digest    string `json:"digest"`
+	} `json:"config"`
+	Layers []struct {
+		MediaType string `json:"mediaType"`
+		Size      int    `json:"size"`
+		Digest    string `json:"digest"`
+	} `json:"layers"`
+}
+
+type TokenResponse struct {
+	Token       string    `json:"token"`
+	AccessToken string    `json:"access_token"`
+	ExpiresIn   int       `json:"expires_in"`
+	IssuedAt    time.Time `json:"issued_at"`
+}
+
+const (
+	getTokenURL    = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:%s:pull"
+	getManifestURL = "https://registry-1.docker.io/v2/library/%s/manifests/%s"
+	getLayerURL    = "https://registry-1.docker.io/v2/library/%s/blobs/%s"
+
+	contentTypeHeader = "application/vnd.docker.distribution.manifest.v2+json"
+)
+
+func getManifest(image, token string) (ManifestResponse, error) {
+	var manifestResponse ManifestResponse
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf(getManifestURL, image, "latest"), nil)
+	if err != nil {
+		return manifestResponse, err
+	}
+
+	req.Header.Set("Accept", contentTypeHeader)
+
+	if token != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return manifestResponse, err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return manifestResponse, fmt.Errorf("cannot get manifestResponse, status: %s", res.Status)
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&manifestResponse); err != nil {
+		return manifestResponse, err
+	}
+
+	return manifestResponse, nil
+}
+
+func getToken(image string) (TokenResponse, error) {
+	var tokenResponse TokenResponse
+
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf(getTokenURL, image), nil)
+	if err != nil {
+		return tokenResponse, err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return tokenResponse, err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return tokenResponse, fmt.Errorf("cannot get tokenResponse, status: %s", res.Status)
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&tokenResponse); err != nil {
+		return tokenResponse, err
+	}
+
+	return tokenResponse, nil
+}
+
+func downloadLayer(image, token, digest string) error {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf(getLayerURL, image, digest), nil)
+	if err != nil {
+		return err
+	}
+
+	if token != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("cannot get layer, status: %s", res.Status)
+
+	}
+
+	// save the layer to a file
+	out, err := os.Create(fmt.Sprintf("%s.tar.gz", digest[7:]))
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	if _, err = io.Copy(out, res.Body); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func extractLayer(src, dest string) error {
+	cmd := exec.Command("tar", "-xvzf", src, "-C", dest)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
+	imageName := os.Args[2]
+
+	// Create temp dir
+	temp, err := os.MkdirTemp("", "docker")
+	if err != nil {
+		log.Printf("cannot create temp dir, error: %v", err)
+		os.Exit(1)
+	}
+
 	// isolateFilesystem will isolate the filesystem with chroot
-	if err := isolateFilesystem(command); err != nil {
+	if err := isolateFilesystem(temp, command); err != nil {
 		log.Printf("cannot isolate filesystem, error: %v", err)
+		os.Exit(1)
+	}
+
+	// Get token
+	tokenResponse, err := getToken(imageName)
+	if err != nil {
+		log.Printf("cannot get token, error: %v", err)
+		os.Exit(1)
+	}
+
+	// Get manifest
+	manifestResponse, err := getManifest(imageName, tokenResponse.Token)
+	if err != nil {
+		log.Printf("cannot get manifest, error: %v", err)
+		os.Exit(1)
+	}
+
+	// Download first layer
+	if err := downloadLayer(imageName, tokenResponse.Token, manifestResponse.Layers[0].Digest); err != nil {
+		log.Printf("cannot download layer, error: %v", err)
+		os.Exit(1)
+	}
+
+	// Extract first layer
+	if err := extractLayer(fmt.Sprintf("%s.tar.gz", manifestResponse.Layers[0].Digest[7:]), temp); err != nil {
+		log.Printf("cannot extract layer, error: %v", err)
 		os.Exit(1)
 	}
 
@@ -48,21 +223,15 @@ func main() {
 	}
 }
 
-func isolateFilesystem(command string) error {
-	// Create temp dir
-	temp, err := os.MkdirTemp("", "docker")
-	if err != nil {
-		return err
-	}
-
+func isolateFilesystem(dir, command string) error {
 	// Copy the command binary to temp dir
-	if _, err := copy(command, temp+command); err != nil {
-		return fmt.Errorf("cannot copy command %s to %s, error: %v", command, temp, err)
+	if _, err := copy(command, dir+command); err != nil {
+		return fmt.Errorf("cannot copy command %s to %s, error: %v", command, dir, err)
 	}
 
 	// Chroot to temp dir
-	if err := syscall.Chroot(temp); err != nil {
-		return fmt.Errorf("cannot chroot to %s, error: %v", temp, err)
+	if err := syscall.Chroot(dir); err != nil {
+		return fmt.Errorf("cannot chroot to %s, error: %v", dir, err)
 	}
 
 	return nil
